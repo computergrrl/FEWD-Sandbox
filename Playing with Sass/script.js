866e47cb08f13d327a7db95eea9ca0996d36566b
computergrrl <anani.mowse@gmail.com>
2021-01-31 14:36:28 -0800

    Commit sandbox files to repo

---
+++ Playing with Sass/script.js
@@ -0,0 +1,25 @@
+const container = document.getElementById("container");
+
+for (let i=1; i<=10; i++) {
+    container.innerHTML += `<div id="box-${i}">Box ${i} </div>`;
+}
+
+
+
+// const name = "Trish";
+// function setName() {
+//   const name = "Sarah";
+// }
+// setName();
+// alert(name);
+
+//Answer = “Trish”
+
+// let message = "Welcome!";
+// function setMessage() {
+//  let message = "Goodbye!";
+// }
+// setMessage();
+// alert(message);
+
+//Answer = “Goodbye”
